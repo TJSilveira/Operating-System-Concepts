5179e07b046118371b9c23f16ef7c612224d70c7
Tiago Silveira <tiagojesusilveira@gmail.com>
2025-06-11 20:11:31 +0200

    Trying out different creation thread techniques

---
+++ Ch04/time.c
@@ -0,0 +1,17 @@
+#include <sys/time.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	struct timeval tv;
+	int i = 0;
+	
+	while (i < 1000)
+	{
+		gettimeofday(&tv, NULL);
+		printf("%ld\n",tv.tv_sec);
+		printf("%ld\n",tv.tv_usec);
+		i++;
+	}
+}
