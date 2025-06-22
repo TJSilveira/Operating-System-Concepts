5e93b5725cd95c48f220a8f2263bbd1eff1e3aaa
Tiago Silveira <tiagojesusilveira@gmail.com>
2025-06-22 12:11:28 +0200

    Finalize the exercise. Create the functions and threads to handle the 3x3 squares

--- Ch04/sudoku_checker.c
+++ Ch04/sudoku_checker.c
@@ -4,6 +4,13 @@
 #include <time.h>
 #include <unistd.h>
 
+typedef struct s_matrix
+{
+	int	(*x)[9];
+	int	i;
+}	t_matrix;
+
+
 int	checker(int *arr)
 {
 	int i;
@@ -21,7 +28,7 @@ int	checker(int *arr)
 		if (sol[i] != 0)
 		{
 			printf("%i %i %i %i %i %i %i %i %i\n", arr[0],arr[1],arr[2],arr[3],arr[4],arr[5],arr[6],arr[7],arr[8]);
-			printf("Error: Row does not have a solution. Program was exited\n");
+			printf("Error: Invalid solution. Program was exited\n");
 			exit (EXIT_FAILURE);
 		}
 		i++;
@@ -39,7 +46,6 @@ void	*row_checker(void *param)
 	row = 0;
 	while (row < 9)
 	{
-		printf("beginnig of the loop\n");
 		col = 0;
 		while (col < 9)
 		{
@@ -76,70 +82,77 @@ void	*col_checker(void *param)
 	return (NULL);
 }
 
-void	matrix_loader(int *arr[9], int i)
+void	*matrix_checker(void *param)
 {
 	int row;
 	int col;
 	int j;
 	int k;
+	int l;
+	int	arr_to_check[9];
+	t_matrix *matrix = (t_matrix*) param;
 
-	if (i == 0)
+	if (matrix->i == 0)
 	{
 		row = 0;
 		col = 0;
 	}
-	else if(i == 1)
+	else if(matrix->i == 1)
 	{
 		row = 3;
 		col = 0;
 	}
-	else if(i == 2)
+	else if(matrix->i == 2)
 	{
 		row = 6;
 		col = 0;
 	}
-	else if(i == 3)
+	else if(matrix->i == 3)
 	{
 		row = 0;
 		col = 3;
 	}
-	else if(i == 4)
+	else if(matrix->i == 4)
 	{
 		row = 3;
 		col = 3;
 	}
-	else if(i == 5)
+	else if(matrix->i == 5)
 	{
 		row = 6;
 		col = 3;
 	}
-	else if(i == 6)
+	else if(matrix->i == 6)
 	{
 		row = 0;
 		col = 6;
 	}
-	else if(i == 7)
+	else if(matrix->i == 7)
 	{
 		row = 3;
 		col = 6;
 	}
-	else if(i == 8)
+	else if(matrix->i == 8)
 	{
 		row = 6;
 		col = 6;
 	}
 	
+	l = 0;
 	j = 0;
 	while (j < 3)
 	{
 		k = 0;
 		while (k < 3)
 		{
-			/* code */
+			arr_to_check[l++] = matrix->x[row + j][col + k];
+			k++;
 		}
-		
+		j++;
 	}
-	
+	checker(arr_to_check);
+	printf("Matrix %i is successful\n", matrix->i);
+	return (NULL);
 }
 
 int	main()
@@ -150,12 +163,21 @@ int	main()
 							{9,6,7,1,4,5,3,2,8},
 							{3,7,2,4,6,1,5,8,9},
 							{6,9,1,5,8,3,2,7,4},
-							{4,5,8,7,9,2,6,1,2},
+							{4,5,8,7,9,2,6,1,3},
 							{8,3,6,9,2,4,1,5,7},
 							{2,1,9,8,5,7,4,3,6},
 							{7,4,5,3,1,6,8,9,2}};
 	int			i;
+	t_matrix	matrix_arr[9];
 
+	i = 0;
+	while (i < 9)
+	{
+		matrix_arr[i].i = i;
+		matrix_arr[i].x = x;
+		i++;
+	}
+	
 	i = 0;
 	// Rows
 	pthread_create(&arr[i], NULL, &row_checker, x);
@@ -163,19 +185,17 @@ int	main()
 	// column
 	pthread_create(&arr[i], NULL, &col_checker, x);
 	i++;
-	// while (i < 11)
-	// {
-	// 	pthread_create(arr[i++], NULL, col_checker, x);
-	// 	i++;
-	// }
+	while (i < 11)
+	{
+		pthread_create(&arr[i], NULL, &matrix_checker, &matrix_arr[i - 2]);
+		i++;
+	}
 
-	pthread_join(arr[0], NULL);
-	pthread_join(arr[1], NULL);
-
-	// i = 0;
-	// while (i < 11)
-	// {
-	// 	pthread_join(arr[i], NULL);
-	// }
-	// return (EXIT_SUCCESS);
+	i = 0;
+	while (i < 11)
+	{
+		pthread_join(arr[i], NULL);
+		i++;
+	}
+	return (EXIT_SUCCESS);
 }
